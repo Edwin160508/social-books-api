5fac5a5f4d2b134248667dc75e8aad20386c55ca
Edwin Lima <edwin.limaa@gmail.com>
2019-09-01 20:09:20 -0300

    Criando Recurso de evento ApplicationEvent, criando metodo salvar no LivroResource.

--- src/main/java/com/example/socialbooks/resources/LivroResources.java
+++ src/main/java/com/example/socialbooks/resources/LivroResources.java
@@ -2,13 +2,21 @@ package com.example.socialbooks.resources;
 
 import java.util.List;
 
+import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.socialbooks.domain.Livro;
+import com.example.socialbooks.event.RecursoCriadoEvent;
 import com.example.socialbooks.services.LivroService;
 
 @RestController
@@ -17,9 +25,22 @@ public class LivroResources {
 	
 	@Autowired
 	private LivroService livroService;
+	
+	@Autowired
+	private ApplicationEventPublisher publisher;
 
 	@GetMapping
 	public ResponseEntity<List<Livro>> listar() {		
 		return ResponseEntity.ok().body(livroService.listar());
 	}
+	
+	@PostMapping
+	public ResponseEntity<Livro> cadastrar(@Valid @RequestBody Livro livro, HttpServletResponse response){
+		Livro livroSalvo = livroService.cadastrar(livro);
+		publisher.publishEvent(new RecursoCriadoEvent(this, response, livroSalvo.getId()));
+		
+		return ResponseEntity.status(HttpStatus.CREATED).body(livroSalvo);
+		
+	} 
+	
 }
