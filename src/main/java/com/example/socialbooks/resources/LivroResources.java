fc23d9c6ccb30f7cae1c87de9759a45a179c9559
Edwin Lima <edwin.limaa@gmail.com>
2019-09-01 17:41:25 -0300

    Usando getMapping annotation.

--- src/main/java/com/example/socialbooks/resources/LivroResources.java
+++ src/main/java/com/example/socialbooks/resources/LivroResources.java
@@ -3,16 +3,17 @@ package com.example.socialbooks.resources;
 import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.socialbooks.domain.Livro;
 
 @RestController
+@RequestMapping("/api/livros")
 public class LivroResources {
 
-	@RequestMapping(value ="/livros", method = RequestMethod.GET)
+	@GetMapping
 	public List<Livro> listar() {
 		Livro l1 = new Livro("Rest aplicado");
 		Livro l2 = new Livro("Git passso-a-passo");
