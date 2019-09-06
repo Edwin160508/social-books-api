25c05aa962a391e7440fedf316f09e182e648e66
Edwin Lima <edwin.limaa@gmail.com>
2019-09-05 23:04:56 -0300

    Retornando location apos cadastrar livro.

--- src/main/java/com/app/socialbooks/resources/LivroResources.java
+++ src/main/java/com/app/socialbooks/resources/LivroResources.java
@@ -1,5 +1,6 @@
 package com.app.socialbooks.resources;
 
+import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 
@@ -8,16 +9,16 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.app.socialbooks.domain.Livro;
 import com.app.socialbooks.event.RecursoCriadoEvent;
@@ -42,8 +43,10 @@ public class LivroResources {
 	public ResponseEntity<Livro> cadastrar(@Valid @RequestBody Livro livro, HttpServletResponse response){
 		Livro livroSalvo = livroService.cadastrar(livro);
 		publisher.publishEvent(new RecursoCriadoEvent(this, response, livroSalvo.getId()));
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+				.path("/{id}").buildAndExpand(livroSalvo.getId()).toUri();
 		
-		return ResponseEntity.status(HttpStatus.CREATED).body(livroSalvo);
+		return ResponseEntity.created(uri).build();
 		
 	} 
 	
