22203988dc8032321e8aaa29e074c9004fe4e2ff
Edwin Lima <edwin.limaa@gmail.com>
2019-09-15 14:03:25 -0300

    Criando recurso de cadastro do Autor.

--- src/main/java/com/app/socialbooks/handler/ResourceExceptionHandler.java
+++ src/main/java/com/app/socialbooks/handler/ResourceExceptionHandler.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import com.app.socialbooks.domain.DetalheErro;
+import com.app.socialbooks.service.exeption.AutorExistenteException;
+import com.app.socialbooks.service.exeption.AutorNaoEncontradoException;
 import com.app.socialbooks.service.exeption.LivroNaoEncontradoException;
 
 /**
@@ -35,4 +37,47 @@ public class ResourceExceptionHandler {
 		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(detalheErro);
 	}
 	
+	/**
+	 * 
+	 * Método responsável por tratar erro de negocio caso na tentativa
+	 * de cadastrar Autor que já existe na base de dados.
+	 * 
+	 * @param e
+	 * @param request
+	 * @return ResponseEntity
+	 */
+	
+	@ExceptionHandler(AutorExistenteException.class)
+	public ResponseEntity<DetalheErro> handlerAutorExistenteException
+							(AutorExistenteException e, HttpServletRequest request){
+		
+			DetalheErro detalheErro = new DetalheErro();
+			detalheErro.setStatus(409l);
+			detalheErro.setTitulo("Esse autor já existe na base de dados.");
+			detalheErro.setMenssagemDesenvolvedor("https://erros.socialbooks.com/409");
+			detalheErro.setDataHora(System.currentTimeMillis());
+			
+		return ResponseEntity.status(HttpStatus.CONFLICT).body(detalheErro);
+	}
+	
+	/**
+	 * 
+	 * Método responsável por tratar erro caso não encontre Autor na base de dados.
+	 * 
+	 * @param e
+	 * @param request
+	 * @return ResponseEntity
+	 */
+	@ExceptionHandler(AutorNaoEncontradoException.class)
+	public ResponseEntity<DetalheErro> handlerAutorNaoEncontradoException
+							(AutorNaoEncontradoException e, HttpServletRequest request){
+		
+			DetalheErro detalheErro = new DetalheErro();
+			detalheErro.setStatus(404l);
+			detalheErro.setTitulo("Autor não encontrado na base de dados.");
+			detalheErro.setMenssagemDesenvolvedor("https://erros.socialbooks.com/404");
+			detalheErro.setDataHora(System.currentTimeMillis());
+			
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(detalheErro);
+	}
 }
