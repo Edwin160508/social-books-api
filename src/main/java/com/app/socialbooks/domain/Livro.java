b3126a4c607ee5d29aa06da43c1a12bd67424619
Edwin Lima <edwin.limaa@gmail.com>
2019-09-15 11:29:23 -0300

    Evoluindo atributo Autor para entidade, criando estrutura de camadas referente a Autor.

--- src/main/java/com/app/socialbooks/domain/Livro.java
+++ src/main/java/com/app/socialbooks/domain/Livro.java
@@ -4,12 +4,16 @@ import java.util.Date;
 import java.util.List;
 
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.Transient;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
 
@@ -36,8 +40,10 @@ public class Livro {
 	@OneToMany(mappedBy = "livro")
 	private List<Comentario> comentarios;
 	
-	@JsonInclude(Include.NON_NULL)
-	private String autor;
+	@ManyToOne
+	@JoinColumn(name = "AUTOR_ID")
+	@JsonIgnore
+	private Autor autor;
 
 	public Livro () {}
 	
@@ -93,11 +99,11 @@ public class Livro {
 		this.comentarios = comentarios;
 	}
 
-	public String getAutor() {
+	public Autor getAutor() {
 		return autor;
 	}
 
-	public void setAutor(String autor) {
+	public void setAutor(Autor autor) {
 		this.autor = autor;
 	}
 	
