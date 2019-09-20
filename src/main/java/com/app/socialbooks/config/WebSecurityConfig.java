4c9e1c5ec2d602b6e8ae6f7ee06f97a26b508d80
Edwin Lima <edwin.limaa@gmail.com>
2019-09-20 11:33:43 -0300

    colocando contextPath na aplicacao, configurando autenticao Spring 5

--- src/main/java/com/app/socialbooks/config/WebSecurityConfig.java
+++ src/main/java/com/app/socialbooks/config/WebSecurityConfig.java
@@ -1,39 +1,37 @@
 package com.app.socialbooks.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	/**
-	 * Método responsável por configurar a autenticação da aplicação.
-	 * -Nesse exemplo será configurado usuario em memória.
+	 * Método responsável por configurar a autenticação da aplicação. -Nesse exemplo
+	 * será configurado usuario em memória.
 	 * 
 	 * @param AuthenticationManagerBuilder
 	 */
-	@Autowired
-	public void configureGlobal(AuthenticationManagerBuilder auth)throws Exception {
-		auth.inMemoryAuthentication().withUser("algaworks")
-		.password("s3nh4").roles("USER");
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+
+		auth.inMemoryAuthentication().withUser("admin").password(encoder.encode("admin")).roles("USER");
 	}
-	
+
 	/**
 	 * Método responsável por configurar acesso a fazer requisições na aplicação
 	 * 
 	 * @param HttpSecurity
 	 */
-	protected void configure(HttpSecurity http) throws Exception{
-		http
-		.authorizeRequests()
-			.antMatchers("/h2-console/**").permitAll()
-			.anyRequest().authenticated()
-		.and()
-			.httpBasic()
-		.and()
-			.csrf().disable();
+	protected void configure(HttpSecurity http) throws Exception {
+		http.formLogin().loginProcessingUrl("/login").permitAll().and().authorizeRequests()
+				.antMatchers("/h2-console/**").permitAll().anyRequest().authenticated().and().httpBasic().and().csrf()
+				.disable();
 	}
+
 }
